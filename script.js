01105d511ebd14e696366675584843a4cd344dc3
ahmednooryadi <ahmednooryadi@gmail.com>
2026-02-21 17:04:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    "https://media1.tenor.com/m/SFy5Za0DyMEAAAAC/erm-fingers.gif",    // 0 normal
+    "https://media1.tenor.com/m/lUU0bTz8YPEAAAAC/dudu-cute-nice.gif",  // 1 confused
+    "https://media1.tenor.com/m/GkVtcIF8IA8AAAAC/tkthao219-bubududu.gif",             // 2 pleading
+    "https://media1.tenor.com/m/HQR0DYdWKTAAAAAC/dudu-king.gif",             // 3 sad
+    "https://media1.tenor.com/m/sWXhCC4A2woAAAAC/bubu-bubu-dudu.gif",       // 4 sadder
+    "https://media1.tenor.com/m/Qu6GUg0Yx90AAAAC/mocha-cry.giff",             // 5 devastated
+    "https://media1.tenor.com/m/HIq9_NvdTgIAAAAC/bubu-dudu-bubu.gif",               // 6 very devastated
+    "https://media1.tenor.com/m/AQU6mkG6dssAAAAC/dudu-angry-dudu-stop-it.gif"  // 7 crying runaway
 ]
 
 const noMessages = [
